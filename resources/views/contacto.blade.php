88e03bea540286fafd1ee43f1c57fc56cd09fa93
jose-rubio-a <jose.rubio5317@alumnos.udg.mx>
2023-09-05 16:42:58 -0600

    Se agrego la ruta Contacto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de Contacto</h1>
+    <form>
+        <label>Correo</label><br>
+        <input type="email" name="email" class="email"><br>
+        <label>Comentario</label><br>
+        <textarea name="comentario" cols="30" rows="10"></textarea><br>
+        <button type="submit" class="submit">Submit</button>
+    </form>
+</body>
+</html>
